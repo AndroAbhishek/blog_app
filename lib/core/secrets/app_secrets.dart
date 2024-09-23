a94928f038656413b82c3d14b11ba6e735463df0
Abhishek Sharma <sharmaabhi4433@gmail.com>
2024-09-23 15:05:07 +0530

    first commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = "https://bqoawfbrzppbnwskxddn.supabase.co";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxb2F3ZmJyenBwYm53c2t4ZGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxNTA1NzcsImV4cCI6MjAzOTcyNjU3N30.9ScWnZ7bIWFPywg4-eTiV8uYbUjHig7my_LmQQFMFGM";
+}
